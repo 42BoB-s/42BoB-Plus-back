6e0081e6ac83b8a232e03cc7936be0dcca9e257b
minju Hong <70425378+HONGMINJU@users.noreply.github.com>
2021-10-13 19:04:02 +0900

    Chore : https적용

--- src/main/java/com/bobPlus/ProjectProtoTypeApplication.java
+++ src/main/java/com/bobPlus/ProjectProtoTypeApplication.java
@@ -9,5 +9,13 @@ public class ProjectProtoTypeApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProjectProtoTypeApplication.class, args);
     }
-
+    
+	private Connector httpToHttpsRedirectConnector() {
+		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+		connector.setScheme("http");
+		connector.setPort(8080);
+		connector.setSecure(false);
+		connector.setRedirectPort(8443);
+		return connector;
+	}
 }
