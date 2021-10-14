d069593b85996da324b9d0f474d3da91e33b2a95
tjeong <53824764+jts8257@users.noreply.github.com>
2021-10-14 19:56:00 +0900

    Fix : url error

--- src/main/java/com/bobPlus/config/InterceptorConfig.java
+++ src/main/java/com/bobPlus/config/InterceptorConfig.java
@@ -16,7 +16,7 @@ public class InterceptorConfig implements WebMvcConfigurer {
                 .order(1)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/login", "/logout", "/", "/42OAuth",
-                        "bobs/rooms", "bobs/mypage", "bobs/header",
+                        "/bobs/rooms", "/bobs/myroom", "/bobs/header",
                         "/css/**", "/*.ico", "/error");
     }
 }
