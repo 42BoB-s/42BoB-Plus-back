6bda6767b3ef53755db52a963e16351c4ceb7748
minju Hong <70425378+HONGMINJU@users.noreply.github.com>
2021-11-07 02:13:33 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,10 +11,10 @@ sudo fuser -k 443/tcp
 sleep 3
 echo "> 새 애플리케이션 배포"
 cd $REPOSITORY/zip/build/libs
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | head -n 1)
+JAR_NAME=$(ls -tr ./*.jar | head -n 1)
 echo "> JAR Name: $JAR_NAME"
 echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME 
 echo "> $JAR_NAME 실행"
-nohup sudo java -jar  -Djasypt.encryptor.password=$ENCRYPT_KEY\
+nohup sudo java -jar -Djasypt.encryptor.password=$ENCRYPT_KEY\
  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
