367a9189204e66cecfe3cc6dad6e715d403d2a4a
minju Hong <70425378+HONGMINJU@users.noreply.github.com>
2021-11-07 02:06:33 +0900

    fix : jar실행 디렉토리 변경 & jar파일 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,7 +10,8 @@ echo "> 8443포트 kill"
 sudo fuser -k 443/tcp
 sleep 3
 echo "> 새 애플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+cd $REPOSITORY/zip/build/libs
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | head -n 1)
 echo "> JAR Name: $JAR_NAME"
 echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME 
