b83e0c40d1df4678504a7fa62cffaa0516ca9692
minju Hong <70425378+HONGMINJU@users.noreply.github.com>
2021-11-07 01:50:37 +0900

    Fix : java jar 복호화 설정 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-REPOSITORY=/home/ubuntu/app/step2
+REPOSITORY=/home/ubuntu/deploy
 PROJECT_NAME=bobsPlus
 echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
@@ -7,7 +7,7 @@ echo "> 8080포트 kill"
 sudo fuser -k 8080/tcp
 sleep 3
 echo "> 8443포트 kill"
-sudo fuser -k 8443/tcp
+sudo fuser -k 443/tcp
 sleep 3
 echo "> 새 애플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
@@ -15,5 +15,5 @@ echo "> JAR Name: $JAR_NAME"
 echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME 
 echo "> $JAR_NAME 실행"
-nohup sudo java -jar \
+nohup sudo java -jar  -Djasypt.encryptor.password=$ENCRYPT_KEY\
  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
