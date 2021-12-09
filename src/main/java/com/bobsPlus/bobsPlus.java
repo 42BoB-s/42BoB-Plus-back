b94d353042a4054dd2a347a65bff0ed79d0b8655
root <root@ip-172-31-37-91.ap-northeast-2.compute.internal>
2021-12-09 21:12:27 +0900

    fix : 함수 추가

--- src/main/java/com/bobsPlus/bobsPlus.java
+++ src/main/java/com/bobsPlus/bobsPlus.java
@@ -1,9 +1,14 @@
 package com.bobsPlus;
 
+import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class bobsPlus {
@@ -11,6 +16,27 @@ public class bobsPlus {
     public static void main(String[] args) {
         SpringApplication.run(bobsPlus.class, args);
     }
+
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		// Enable SSL Trafic
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+
+		// Add HTTP to HTTPS redirect
+		tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+
+		return tomcat;
+	}
 
 	private Connector httpToHttpsRedirectConnector() {
 		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
