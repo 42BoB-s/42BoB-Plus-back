e343fbe740e5bd8f68df90eeb6dc46ca7f885595
root <root@ip-172-31-37-91.ap-northeast-2.compute.internal>
2021-12-09 20:59:16 +0900

    fix : 함수 추가

--- src/main/java/com/bobsPlus/bobsPlus.java
+++ src/main/java/com/bobsPlus/bobsPlus.java
@@ -11,12 +11,33 @@ public class bobsPlus {
     public static void main(String[] args) {
         SpringApplication.run(bobsPlus.class, args);
     }
-    
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        // Enable SSL Trafic
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+
+        // Add HTTP to HTTPS redirect
+        tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+
+        return tomcat;
+    }
+ 
 	private Connector httpToHttpsRedirectConnector() {
 		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
 		connector.setScheme("http");
 		connector.setPort(8080);
-       	connector.setSecure(false);
+	       	connector.setSecure(false);
 		connector.setRedirectPort(443);
 		return connector;
 	}
