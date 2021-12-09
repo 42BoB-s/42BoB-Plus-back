4c0bf873c2eeada472c705fcfec305ef8e64d679
root <root@ip-172-31-37-91.ap-northeast-2.compute.internal>
2021-12-09 21:03:29 +0900

    fix : 함수 삭제

--- src/main/java/com/bobsPlus/bobsPlus.java
+++ src/main/java/com/bobsPlus/bobsPlus.java
@@ -12,27 +12,6 @@ public class bobsPlus {
         SpringApplication.run(bobsPlus.class, args);
     }
 
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-        // Enable SSL Trafic
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-
-        // Add HTTP to HTTPS redirect
-        tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
-
-        return tomcat;
-    }
- 
 	private Connector httpToHttpsRedirectConnector() {
 		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
 		connector.setScheme("http");
