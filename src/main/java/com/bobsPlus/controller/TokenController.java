e1e6a6f25dee97a8f3ad5e039f941d249e5bbc91
dohelee <leeworld9@gmail.com>
2021-12-15 08:17:35 +0900

    feat : 개발용 jwt 임시토큰 발행 api 추가

--- src/main/java/com/bobsPlus/controller/TokenController.java
+++ src/main/java/com/bobsPlus/controller/TokenController.java
@@ -1,8 +1,10 @@
 package com.bobsPlus.controller;
 
+import com.bobsPlus.dto.UserDto;
 import com.bobsPlus.service.TokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 
 @RequiredArgsConstructor
 @RestController
@@ -27,4 +30,22 @@ public class TokenController {
         -> Authorization : Bearer {jwt access token}
            ex) Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsb2dpbiI6ImRvaGVsZWUiLCJlbWFpbCI6ImRvaGVsZWVAc3R1ZGVudC40MnNlb3VsLmtyIiwiaW1hZ2VfdXJsIjoiaHR0cHM6Ly9jZG4uaW50cmEuNDIuZnIvdXNlcnMvZG9oZWxlZS5qcGciLCJyb2xlIjoiVVNFUiIsInN1YiI6InVzZXItYXV0aCIsImlhdCI6MTYzNzE4NTM2OSwiZXhwIjoxNjM3MTg1OTY5fQ.luZiLo1SzSMkVX-2uAaNSaVUGhNEmu5mERPUzDSUDBM
      */
+
+    //임시 토큰 발급
+    @GetMapping("/bobs/header")
+    public ResponseEntity<HashMap<String, String>> tmpToken()
+    {
+        ResponseEntity<HashMap<String, String>> entity;
+        UserDto userDto = UserDto.builder()
+                .id("testUser")
+                .email("testUser@test.com")
+                .profile("https://www.google.co.jp/logos/doodles/2021/seasonal-holidays-2021-6753651837109324.3-ladc.gif")
+                .build();
+        String token = tokenService.generateToken(userDto, "USER");
+        HashMap<String, String> auth = new HashMap<>();
+        auth.put("Authorization", token);
+        entity = new ResponseEntity<>(auth, HttpStatus.FORBIDDEN);
+        return entity;
+    }
+
 }
