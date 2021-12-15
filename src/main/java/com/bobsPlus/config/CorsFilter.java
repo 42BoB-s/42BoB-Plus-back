397dc09be9dce5e3d53c032a1d47fe6ec1b2b57d
dohelee <leeworld9@gmail.com>
2021-12-15 23:03:07 +0900

    fix : CORS preflight error 수정중3

--- src/main/java/com/bobsPlus/config/CorsFilter.java
+++ src/main/java/com/bobsPlus/config/CorsFilter.java
@@ -17,12 +17,6 @@ public class CorsFilter implements Filter {
     {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods","*");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers",
-                "Origin, X-Requested-With, Content-Type, Accept, Key, Authorization");
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
