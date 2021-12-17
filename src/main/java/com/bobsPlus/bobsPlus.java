7ea004805ddebf037b69bd1b97654253345a5eac
dohelee <leeworld9@gmail.com>
2021-12-17 20:26:34 +0900

    fix : WebConfig allowedOrigins 추가

--- src/main/java/com/bobsPlus/bobsPlus.java
+++ src/main/java/com/bobsPlus/bobsPlus.java
@@ -17,33 +17,33 @@ public class bobsPlus {
         SpringApplication.run(bobsPlus.class, args);
     }
 
-	@Bean
-	public ServletWebServerFactory servletContainer() {
-		// Enable SSL Trafic
-		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-			@Override
-			protected void postProcessContext(Context context) {
-				SecurityConstraint securityConstraint = new SecurityConstraint();
-				securityConstraint.setUserConstraint("CONFIDENTIAL");
-				SecurityCollection collection = new SecurityCollection();
-				collection.addPattern("/*");
-				securityConstraint.addCollection(collection);
-				context.addConstraint(securityConstraint);
-			}
-		};
+//	@Bean
+//	public ServletWebServerFactory servletContainer() {
+//		// Enable SSL Trafic
+//		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+//			@Override
+//			protected void postProcessContext(Context context) {
+//				SecurityConstraint securityConstraint = new SecurityConstraint();
+//				securityConstraint.setUserConstraint("CONFIDENTIAL");
+//				SecurityCollection collection = new SecurityCollection();
+//				collection.addPattern("/*");
+//				securityConstraint.addCollection(collection);
+//				context.addConstraint(securityConstraint);
+//			}
+//		};
+//
+//		// Add HTTP to HTTPS redirect
+//		tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+//
+//		return tomcat;
+//	}
 
-		// Add HTTP to HTTPS redirect
-		tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
-
-		return tomcat;
-	}
-
-	private Connector httpToHttpsRedirectConnector() {
-		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-		connector.setScheme("http");
-		connector.setPort(8080);
-	       	connector.setSecure(false);
-		connector.setRedirectPort(443);
-		return connector;
-	}
+//	private Connector httpToHttpsRedirectConnector() {
+//		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+//		connector.setScheme("http");
+//		connector.setPort(8080);
+//	       	connector.setSecure(false);
+//		connector.setRedirectPort(443);
+//		return connector;
+//	}
 }
