1344d5449d92275b8df79609c04555e7340ed33b
dohelee <leeworld9@gmail.com>
2021-12-17 22:11:41 +0900

    fix : cors filter origin 주소 수정

--- src/main/java/com/bobsPlus/config/SimpleCorsFilter.java
+++ src/main/java/com/bobsPlus/config/SimpleCorsFilter.java
@@ -19,7 +19,7 @@ public class SimpleCorsFilter extends GenericFilterBean {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods","*");
         response.setHeader("Access-Control-Max-Age", "3600");
