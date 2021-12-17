ed069e18f7f8653f93d5ec536bcfe38f7d42c074
dohelee <leeworld9@gmail.com>
2021-12-17 20:45:47 +0900

    fix : https 주석 해제

--- src/main/java/com/bobsPlus/config/WebConfig.java
+++ src/main/java/com/bobsPlus/config/WebConfig.java
@@ -12,8 +12,7 @@ public class WebConfig implements WebMvcConfigurer
     @Override
     public void addCorsMappings(CorsRegistry registry)
     {
-        registry
-                .addMapping("/**")
+        registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("*");
     }
