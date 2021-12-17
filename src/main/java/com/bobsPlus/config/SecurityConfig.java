453e1ece0a2a9176b44657ffe282f59b56e5c310
HONGMINJU <mhong0800@gmail.com>
2021-12-17 21:49:12 +0900

    fix : import CorsUtils

--- src/main/java/com/bobsPlus/config/SecurityConfig.java
+++ src/main/java/com/bobsPlus/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
