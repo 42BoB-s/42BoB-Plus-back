7f447d7c801b1a4661b3b7b1fbd9f9e93336b274
dohelee <leeworld9@gmail.com>
2022-01-04 20:28:46 +0900

    fix : WebSocketConfig setAllowedOrigins 추가

--- src/main/java/com/bobsPlus/config/WebSocketConfig.java
+++ src/main/java/com/bobsPlus/config/WebSocketConfig.java
@@ -15,6 +15,6 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-		registry.addHandler(webSocketHandler,"/bobs/chat");
+		registry.addHandler(webSocketHandler,"/bobs/chat").setAllowedOrigins("*");
 	}
 }
